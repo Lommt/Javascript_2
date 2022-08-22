80c619875c644f94dc6267e242c5998803a8cb14
Leo <leonail1998@gmail.com>
2022-08-22 19:07:22 +0300

    Исправление index, app, style

--- javascript/app.js
+++ javascript/app.js
@@ -1,19 +1,20 @@
-const goods = [
-  { title: "Shirt", price: 150, img: `src="img/item.svg"` },
-  { title: "Socks", price: 50, img: `src="img/item.svg"` },
-  { title: "Jacket", price: 350, img: `src="img/item.svg"` },
-  { title: "Shoes", price: 250, img: `src="img/item.svg"` },
+const products = [
+  { id: 1, title: 'Notebook', price: 2000, img: `src="img/item.svg"` },
+  { id: 2, title: 'Mouse', price: 20, img: `src="img/item.svg"` },
+  { id: 3, title: 'Keyboard', price: 200, img: `src="img/item.svg"` },
+  { id: 4, title: 'Gamepad', price: 50, img: `src="img/item.svg"` },
 ];
-const renderGoodsItem = (x) => {
-  return `<div class="goods-item">
+const renderProduct = (x) => {
+  return `<div class="products-item">
           <img ${x.img} alt="svg">
-          <h3 class="goods-title">${x.title}</h3>
-          <p class="goods-text">${x.price} <span>$</span></p>
-          <button class="goods-btn">Buy</button>
+          <h3 class="products-title">${x.title}</h3>
+          <p class="products-text">${x.price} <span>$</span></p>
+          <button class="products-btn">Buy</button>
           </div>`;
 };
-const renderGoodsList = (list) => {
-  let goodsList = list.map((item) => renderGoodsItem(item));
-  document.querySelector(".goods-list").innerHTML = goodsList.join("");
+const renderPage = list => {
+  let productsList = list.map((item) => renderProduct(item));
+  console.log(productsList);
+  document.querySelector(".products").innerHTML = productsList.join("");
 };
-renderGoodsList(goods);
+renderPage(products);
